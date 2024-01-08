8b61ba317abcf15d2f647e64285bc89ef5069886
Molnár József <mojo12984@gmail.com>
2024-01-08 14:21:59 +0100

    added functions to change value of the progress bars when it changes
    requires testing

--- scenes/stage_control.gd
+++ scenes/stage_control.gd
@@ -1,11 +1,33 @@
 extends HBoxContainer
 
+@onready var floor_progressbar = $FloorProgress
+@onready var monster_progressbar = $MonsterProgress
+@onready var floor_progressbar_label = $FloorProgress/FloorProgressLabel
+@onready var monster_progressbar_label = $MonsterProgress/MonsterProgressLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_progress_fill_color()
+	PlayerData.connect("floor_changed",_on_floor_changed)
+	PlayerData.connect("monster_changed",_on_monster_count_changed)
+	
 
+func set_progress_fill_color():
+	var progress_fill_style = StyleBoxFlat.new()
+	progress_fill_style.bg_color = Color(Color.BLUE)
+	floor_progressbar.add_theme_stylebox_override("fill", progress_fill_style)
+	monster_progressbar.add_theme_stylebox_override("fill", progress_fill_style)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func update_floor_progressbar_text():
+	floor_progressbar_label.text = "Floor: " + str(floor_progressbar.value) + " / " + "100"
+		
+func update_monster_progressbar_text():
+	monster_progressbar_label.text = "Monster: " + str(monster_progressbar.value) + " / " + "15"
+	
+func _on_floor_changed(tower_floor):
+	floor_progressbar.value = tower_floor
+	update_floor_progressbar_text()
+	
+func _on_monster_count_changed(monster_count):
+	monster_progressbar.value = monster_count
+	update_monster_progressbar_text()
