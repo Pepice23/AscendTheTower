1fbed9917a3aedc21be6b62f0a5e312c4164953d
Molnár József <mojo12984@gmail.com>
2024-01-08 14:22:42 +0100

    Added signals, and functions to floor and monster changes
    requires testing

--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -1,5 +1,9 @@
 extends Node
 
+signal  floor_changed
+signal  monster_changed
+
+
 var current_floor = 1
 var current_mosnter = 1
 
@@ -9,6 +13,11 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func on_floor_changed():
+	current_floor = current_floor + 1
+	emit_signal("floor_changed", current_floor)
+
+func change_monster_count():
+	current_mosnter += 1
+	emit_signal("monster_changed", current_mosnter)
