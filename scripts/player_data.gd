243c6eb6cb6832d4001cf3d584fda6c2092b8150
Molnár József <mojo12984@gmail.com>
2024-01-14 17:39:24 +0100

    added player_level and armor_multiplier

--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -2,10 +2,14 @@ extends Node
 
 signal  floor_changed
 signal  monster_changed
+signal  player_level_changed
+signal  armor_multiplier_changed
 
 
 var current_floor = 1
 var current_mosnter = 1
+var player_level = 1
+var armor_multiplier = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,3 +25,12 @@ func on_floor_changed():
 func change_monster_count():
 	current_mosnter += 1
 	emit_signal("monster_changed", current_mosnter)
+
+func change_player_level():
+	player_level += 1
+	emit_signal("player_level_changed", player_level)
+
+func change_armor_multiplier(multiplier):
+	armor_multiplier = multiplier
+	emit_signal("armor_multiplier_changed", armor_multiplier)
+
