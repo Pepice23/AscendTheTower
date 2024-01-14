2ab921b42c5d3a8cad5f13f1e142317752c3e350
Molnár József <mojo12984@gmail.com>
2024-01-14 17:38:52 +0100

    added new autoload WeaponCreator

---
+++ scripts/weapon_creator.gd
@@ -0,0 +1,37 @@
+extends Node
+
+var weapon_qualities = {
+	"Poor": 1,
+	"Uncommon": 2,
+	"Rare": 3,
+	"Epic": 4,
+	"Legendary": 5
+}
+
+var base_damage = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(0, 10):
+		create_weapon()
+
+
+# A function that creates a new weapon with random stats
+func create_weapon():
+	var level = PlayerData.player_level
+	var armor_multiplier = PlayerData.armor_multiplier
+	var level_multiplier = 1 + ((level - 1) * 0.42) ** 6
+	var qualities = weapon_qualities.keys()
+	var random_index = randi() % qualities.size()
+	var weapon_quality = qualities[random_index]
+	var weapon_damage = weapon_qualities[weapon_quality] * base_damage * level_multiplier * armor_multiplier
+	var weapon = {
+		"quality": weapon_quality,
+		"name": weapon_quality + " Weapon",
+		"damage": weapon_damage,
+		"value": weapon_qualities[weapon_quality] * 20
+	}
+	print(weapon)
+	return weapon
+
+	
