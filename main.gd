0c4bf242c250072adef9a7d759c6e98bf6bb1a1e
Molnár József <mojo12984@gmail.com>
2024-01-20 13:30:51 +0100

    added 2 buttons for Enemy HP test

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var animation_player = $BattleAnimation/AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 func _on_add_floor_pressed():
 	PlayerData.on_floor_changed()
@@ -22,7 +22,7 @@ func _on_add_level_pressed():
 
 
 func _on_add_xp_pressed():
-	PlayerData.gain_random_xp(8,22)
+	PlayerData.gain_random_xp(8, 22)
 
 
 func _on_add_damage_pressed():
@@ -31,3 +31,12 @@ func _on_add_damage_pressed():
 
 func _on_add_money_pressed():
 	PlayerData.change_player_money(10)
+
+
+
+func _on_set_enemy_hp_pressed():
+	EnemyData.calculate_enemy_hp()
+
+
+func _on_defeat_enemy_pressed():
+	EnemyData.enemy_defeat()
