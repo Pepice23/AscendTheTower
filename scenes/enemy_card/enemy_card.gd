c1d4036a8b31b545d31b2044b35a0e6925a8b8f8
Molnár József <mojo12984@gmail.com>
2024-01-23 14:26:41 +0100

    enemy card update

--- scenes/enemy_card/enemy_card.gd
+++ scenes/enemy_card/enemy_card.gd
@@ -13,6 +13,8 @@ extends VBoxContainer
 func _ready():
 	EnemyData.connect("enemy_current_hp_changed", _on_current_hp_changed)
 	EnemyData.connect("enemy_max_hp_changed", _on_max_hp_changed)
+	EnemyData.connect("bossfight_current_time_changed", _on_current_time_changed)
+	EnemyData.connect("bossfight_max_time_changed", _on_max_time_changed)
 	PlayerData.connect("show_boss_timer", _on_show_bossfight_timer)
 	PlayerData.connect("hide_boss_timer", _on_hide_bossfight_timer)
 	PlayerData.connect("floor_changed", _on_level_changed)
@@ -27,19 +29,19 @@ func set_defaults():
 
 func set_hp_progress_fill_color():
 	var progress_fill_style = StyleBoxFlat.new()
-	progress_fill_style.bg_color = Color(Color.RED)
+	progress_fill_style.bg_color = Color(Color.DARK_RED)
 	enemy_hp_progress_bar.add_theme_stylebox_override("fill", progress_fill_style)
 
 func set_boss_progress_fill_color():
 	var progress_fill_style = StyleBoxFlat.new()
-	progress_fill_style.bg_color = Color(Color.DARK_RED)
+	progress_fill_style.bg_color = Color(Color.RED)
 	boss_time_progress_bar.add_theme_stylebox_override("fill", progress_fill_style)
 
 func update_hp_progressbar_text():
 	enemy_hp_progress_bar_text.text = Utils.format_number(enemy_hp_progress_bar.value)
 
 func update_boss_progressbar_text():
-	boss_time_progress_bar_text.text = boss_time_progress_bar.value + " seconds left"
+	boss_time_progress_bar_text.text = str(boss_time_progress_bar.value) + " seconds left"
 
 func _on_current_hp_changed(current_hp):
 	enemy_hp_progress_bar.value = current_hp
@@ -51,6 +53,9 @@ func _on_max_hp_changed(max_hp):
 func _on_current_time_changed(current_time):
 	boss_time_progress_bar.value = current_time
 	update_boss_progressbar_text()
+
+func _on_max_time_changed(max_time):
+	boss_time_progress_bar.value = max_time
 
 func _on_level_changed(level):
 	enemy_level_text.text = str(level)
