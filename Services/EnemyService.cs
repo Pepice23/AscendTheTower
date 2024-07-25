6983a49e47b3422de34f17621fc5c836dbc36115
Molnár József <mojo12984@gmail.com>
2024-07-25 11:10:41 +0200

    EnemyStats layout ready

--- Services/EnemyService.cs
+++ Services/EnemyService.cs
@@ -4,4 +4,8 @@ namespace AscendTheTower.Services;
 public class EnemyService
 {
     public event Action OnChange;
+    public long EnemyCurrentHp { get; private set; } = 100;
+    public long EnemyMaxHp { get; private set; } = 100;
+    public int CurrentBossTime { get; private set; } = 30;
+    public int MaxBossTime { get; private set; } = 30;
 }
