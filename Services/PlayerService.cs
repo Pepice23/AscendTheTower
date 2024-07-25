866653702b2a736b37b49fe8d8342d8c5050217a
Molnár József <mojo12984@gmail.com>
2024-07-25 10:06:39 +0200

    Added testing buttons, and methods for Stage

--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -5,12 +5,25 @@ public class PlayerService
 {
     public event Action OnChange;
 
-    public int CurrentFloor { get; set; } = 1;
-    public int CurrentEnemy { get; set; } = 1;
+    public int CurrentFloor { get; private set; } = 1;
+    public int CurrentEnemy { get; private set; } = 1;
 
     public void AddFloor()
     {
         CurrentFloor++;
+        CurrentEnemy = 1; //reset enemy
+        OnChange?.Invoke();
+    }
+
+    public void AddEnemy()
+    {
+        CurrentEnemy++;
+        OnChange?.Invoke();
+    }
+
+    public void ResetEnemy()
+    {
+        CurrentEnemy = 1;
         OnChange?.Invoke();
     }
     
