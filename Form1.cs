3106d191d88800eec73e74097c8f238c9750f2b4
Molnár József <mojo12984@gmail.com>
2024-07-31 10:59:41 +0200

    configuring autoupdater

--- Form1.cs
+++ Form1.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 
 namespace AscendTheTower;
 
+using AutoUpdaterDotNET;
+
 public partial class Form1 : Form
 {
     public Form1()
     {
+        AutoUpdater.Start("https://github.com/Pepice23/AscendTheTower/blob/master/Updater.xml");
         InitializeComponent();
 
         var blazor = new BlazorWebView()
