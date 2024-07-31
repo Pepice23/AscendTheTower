897b0cb3c3afc5759e14cfc55ec1b75a0ca0afd6
Molnár József <mojo12984@gmail.com>
2024-07-31 11:24:37 +0200

    autoupdater did not work

--- Form1.cs
+++ Form1.cs
@@ -2,13 +2,10 @@ using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 
 namespace AscendTheTower;
 
-using AutoUpdaterDotNET;
-
 public partial class Form1 : Form
 {
     public Form1()
     {
-        AutoUpdater.Start("https://github.com/Pepice23/AscendTheTower/blob/master/Updater.xml");
         InitializeComponent();
 
         var blazor = new BlazorWebView()
