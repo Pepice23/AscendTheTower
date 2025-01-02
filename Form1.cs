757ee2da9c038752dabc9e3eea26f8327d1973ab
Molnár József <mojo12984@gmail.com>
2025-01-02 15:03:30 +0100

    fix link

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ namespace AscendTheTower;
 public partial class Form1 : Form
 {
     private readonly AutoUpdater _autoUpdater = new AutoUpdater(
-        "https://gist.github.com/Pepice23/582a32e578ea048ec7dc9c9c510fb59a",
+        "https://gist.githubusercontent.com/Pepice23/582a32e578ea048ec7dc9c9c510fb59a/raw/dddb534d5b30fa7f9a0dee41c2ae2a5de871d6fc/updater.json",
         "1.0.0");
     public Form1()
     {
@@ -24,7 +24,7 @@ public partial class Form1 : Form
         blazor.RootComponents.Add<Main>("#app");
         Controls.Add(blazor);
     }
-    
+
     protected override async void OnLoad(EventArgs e)
     {
         base.OnLoad(e);
