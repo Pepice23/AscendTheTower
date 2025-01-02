1ab8fcf140d6488b2b60686038f4d48f270cf834
Molnár József <mojo12984@gmail.com>
2025-01-02 15:30:03 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ public partial class Form1 : Form
 {
     private readonly AutoUpdater _autoUpdater = new AutoUpdater(
         "https://gist.githubusercontent.com/Pepice23/582a32e578ea048ec7dc9c9c510fb59a/raw/dddb534d5b30fa7f9a0dee41c2ae2a5de871d6fc/updater.json",
-        "2.0.0");
+        "2.0.1");
     public Form1()
     {
         InitializeComponent();
