fb5c5e39dd645002a2cd1af2b9ecfaeb08d74147
Molnár József <mojo12984@gmail.com>
2025-01-02 15:29:23 +0100

    When the game starts the form name will be Ascend the tower
    updater test

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        Text = "Ascend The Tower";
 
         var blazor = new BlazorWebView()
         {
